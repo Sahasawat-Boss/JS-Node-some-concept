4f0722a142b3cd287c7c3e00998ede3a0cb2e2e6
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-07 09:40:21 +0700

    Update Comments

--- project.js
+++ project.js
@@ -1,4 +1,5 @@
-// 1. Despot some money << Done!
+//* Functions in this Project
+//* 1. Despot some money << Done!
 // 2. Determine number of lines to bet on
 // 3. Collect a bet amount
 // 4. Spin the slot machine
@@ -9,7 +10,8 @@
 // function deposit() {
 //     return 1
 // } 
-// **------------------------------------------------------------------------------------------
+
+//*------------------------------------------------------------------------------------------
 
 // Import the 'prompt-sync' package to enable user input from the console.
 const prompt = require("prompt-sync")();
@@ -29,6 +31,7 @@ const deposit = () => {
 
         // Check if the input is a valid number and greater than 0.
         if (isNaN(numberDepositeAnmount) || numberDepositeAnmount <= 0) {
+            // || = or
             // If invalid, print an error message and prompt the user again.
             console.log("Invalid deposit amount, please try again.");
         } else {
@@ -38,11 +41,15 @@ const deposit = () => {
     }
 };
 
+
+//in JavaScript, function definitions do not need to be listed in order,
+// but functions must be called in the correct order to avoid errors.
+//*------------------------------------------------------------------------------------------
+
 // Call the 'deposit' function and store the valid deposit amount in 'depositeAmount' variable.
 const depositeAmount = deposit();
-
-// Print the user's balance after depositing.
 console.log("Your Balance is " + depositeAmount);
+// Print the user's balance after depositing.
 
 // To run this script in the terminal, use the following command:
 // node project.js
