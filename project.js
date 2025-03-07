b8d698bc45f3ab776c6abf884e5c36f2f2ca1930
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-07 09:14:07 +0700

    Initial commit

---
+++ project.js
@@ -0,0 +1,22 @@
+// 1. Despot some money <<
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. check if the user won
+// 6. give the user their winnings
+// 7. play again
+
+// function deposit() {
+//     return 1
+    
+// } 
+
+const prompt = require("prompt-sync")();
+// () >> Give access to that function that we call 
+
+const deposit = () =>{
+    const depositeAmount = prompt("Enter a deposite amount:");
+};
+
+deposit();
+//TERMINAL >node project.js
